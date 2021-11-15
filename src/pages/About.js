03fbd639e72eeb91eee80090440c0e2b2d4657d7
ThanhPhi <thanhphichv96@gmail.com>
2021-11-15 14:00:41 +0700

    add data

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Hi, I'm Lucifer. This Cocktail Web design you are viewing is an independent project and I am solely responsible for it.
+      </p>
+      <p>
+        This is a website I made for Ckery - my lover. She is quite fond of bartending and alcohol, so I think these will be of great help to her.
+      </p> 
+      <p>
+        If you have more information or need to contact me, contact me via <a href="https://www.facebook.com/thang.nguyenquang.18041/">**Facebook**</a> or <a href="https://mail.google.com/mail/u/0/#search/thang.nq1007%40gmail.com">**Gmail**</a>.
+      </p>
+    </section>
+  );
+}
+
+export default About
